0ed6b260afab42e20db2a3c418a0b09c3f028fbd
Dario <dariomadau@hotmail.com>
2023-06-09 15:16:43 +0200

    changed colums and responsiveness

--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -7,7 +7,7 @@ export function LogoCloud() {
         <h2 className="text-center text-lg font-semibold leading-8 text-white">
           Appearing in
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="/logocloud/quote.png"
@@ -32,13 +32,6 @@ export function LogoCloud() {
           <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="/logocloud/RTL.png"
-            alt="logo"
-            width={158}
-            height={48}
-          />
-          <Image
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
             alt="logo"
             width={158}
             height={48}
