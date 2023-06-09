04fe1e9e68f72b5373b0dd5ac308615e73bd0d79
Dario <dariomadau@hotmail.com>
2023-06-09 21:50:41 +0200

    style & text changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,55 +2,10 @@ import Link from 'next/link';
 
 export default function Contact() {
   return (
-    <div className="relative isolate bg-gray-900">
+    <div className="relative isolate bg-stone-900">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-white/5 lg:w-1/2">
-              <svg
-                className="absolute inset-0 h-full w-full stroke-gray-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-                aria-hidden="true"
-              >
-                <defs>
-                  <pattern
-                    id="54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2"
-                    width={200}
-                    height={200}
-                    x="100%"
-                    y={-1}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <path d="M130 200V.5M.5 .5H200" fill="none" />
-                  </pattern>
-                </defs>
-
-                <svg
-                  x="100%"
-                  y={-1}
-                  className="overflow-visible fill-gray-800/20"
-                >
-                  <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
-                </svg>
-                <rect
-                  width="100%"
-                  height="100%"
-                  strokeWidth={0}
-                  fill="url(#54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2)"
-                />
-              </svg>
-              <div
-                className="absolute -left-56 top-[calc(100%-13rem)] transform-gpu blur-3xl lg:left-[max(-14rem,calc(100%-59rem))] lg:top-[calc(50%-7rem)]"
-                aria-hidden="true"
-              >
-                <div
-                  className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-br from-[#80caff] to-[#4f46e5] opacity-20"
-                  style={{
-                    clipPath:
-                      'polygon(74.1% 56.1%, 100% 38.6%, 97.5% 73.3%, 85.5% 100%, 80.7% 98.2%, 72.5% 67.7%, 60.2% 37.8%, 52.4% 32.2%, 47.5% 41.9%, 45.2% 65.8%, 27.5% 23.5%, 0.1% 35.4%, 17.9% 0.1%, 27.6% 23.5%, 76.1% 2.6%, 74.1% 56.1%)',
-                  }}
-                />
-              </div>
-            </div>
             <Link href="/">
               <span className="text-3xl " aria-hidden="true">
                 &larr;
@@ -163,7 +118,7 @@ export default function Contact() {
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
-                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="rounded-md bg-stone-200 px-3.5 py-2.5 text-center text-sm font-semibold text-stone-900 shadow-sm hover:bg-stone-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
               >
                 Send message
               </button>
--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -5,7 +5,7 @@ export function LogoCloud() {
     <div className="bg-stone-900 py-24 ">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg font-semibold leading-8 text-white">
-          Appearing in
+          Appeared in
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           <Image
